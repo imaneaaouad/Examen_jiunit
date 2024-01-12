9bea01bb3c2e7ac862118a4a6b377de93af68588
Salah-Eddine Chakkor <eddinesalah1999@gmail.com>
2024-01-12 18:51:17 +0100

    Implémentation de la teste du modification du produit

--- src/test/java/com/imaneaaouad_salah_edin/ProduitServiceTest.java
+++ src/test/java/com/imaneaaouad_salah_edin/ProduitServiceTest.java
@@ -29,22 +29,22 @@ class ProduitServiceTest {
         assertThrows(ProduitNotFoundException.class, () -> produitService.trouverProduitParId(2L));
     }
 
-    @Test
-    void testMettreAJourProduit() throws ProduitException, ProduitNotFoundException {
-        ProduitService produitService = new ProduitService();
-        Produit produit1 = new Produit(1L, "Produit1", 10.0, 5);
-        produitService.ajouterProduit(produit1);
+       @Test
+       void testMettreAJourProduit() throws ProduitException, ProduitNotFoundException {
+          ProduitService produitService = new ProduitService();
+          Produit produit1 = new Produit(1L, "Produit1", 10.0, 5);
+          produitService.ajouterProduit(produit1);
 
-        produit1.setPrix(15.0);
-        produit1.setQuantite(8);
-        produitService.mettreAJourProduit(produit1);
+          produit1.setPrix(15.0);
+          produit1.setQuantite(8);
+          produitService.mettreAJourProduit(produit1);
 
-        Produit produitMisAJour = (Produit) produitService.trouverProduitParId(1L);
-        assertEquals(15.0, produitMisAJour.getPrix());
-        assertEquals(8, produitMisAJour.getQuantite());
-
-        assertThrows(ProduitNotFoundException.class, () -> produitService.mettreAJourProduit(new Produit(2L, "Produit2", 20.0, 3)));
-    }
+          Produit produitMisAJour = (Produit) produitService.trouverProduitParId(1L);
+          assertEquals(15.0, produitMisAJour.getPrix());
+          assertEquals(8, produitMisAJour.getQuantite());
+ 
+          assertThrows(ProduitNotFoundException.class, () -> produitService.mettreAJourProduit(new Produit(2L, "Produit2", 20.0, 3)));
+         }
 
     @Test
     void testSupprimerProduit() throws ProduitNotFoundException, ProduitException {
